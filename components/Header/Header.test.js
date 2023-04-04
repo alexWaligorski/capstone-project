025bfcb6a75cac0828cbf9f79b394c8e02178047
Alexandra Waligorski <123748272+alexWaligorski@users.noreply.github.com>
2023-04-04 16:16:51 +0200

    Update components/Header/Header.test.js
    Co-authored-by: Jan Willem Henckel <jan@mpfilm.de>

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import { render, screen } from "@testing-library/react";
 
-test("test", () => {
+test("renders the title", () => {
   render(<Header />);
   const element = screen.getByText("DogDates");
   expect(element).toBeInTheDocument();
